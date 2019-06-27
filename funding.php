985a8547a1bfdc275713ed94462340223e18d06a
Jasmine Vout <47492753+jasminevout@users.noreply.github.com>
2019-06-27 16:49:03 +0800

    Add Funding Content

--- funding.php
+++ funding.php
@@ -1,7 +1,28 @@
 <?php 
 $pageTitle ="Funding";
 include 'templates/header.php';?>
-<main>
-<p>Page Awaiting Content</p>
+<main class="grid-x">
+    <!-- LEFT COLUMN -->
+    <div class="column column-left cell small-12 medium-8">
+        <section class="mod cell small-12">
+            <h1>Funding</h1>
+            <p>Revenues gained through membership subscriptions, fund raising activities, publications sales, gifts, bequests (such as the Sholl Bequest), and grants are spent on various projects submitted by staff of the State Library of Western Australia and the State Records as well by members of our organisation to the Friends' Committee for selection each year.</p>
+            <p>Large projects have required extra effort including applications for large external grants. The last of these was funds of $3 million from Lotterywest, supported by the consortium of historical and community organisations and private donors, to rescue and preserve unique and irreplaceable materials in the Battye Library.</p>
+
+            <h2>Maude Sholl Bequest</h2>
+            <p>Maude Sholl was a well-wisher who left a substatial bequest for the use of the Friends of Battye Library Inc.</p>
+            <p>Monies are paid to the Friends for them to use for the complication of funding aids and the editing of documents with a view to publication and otherwise for the general purposed and objects of the Battye Library of Western Australian History. The Sholl Bequest is our major source of regular funding.</p>
+        </section>
+    </div> <!-- end .column-left -->
+    
+    <!-- RIGHT COLUMN -->
+    <div class="column column-right cell small-12 medium-4">
+        <section class="mod cell small-12">
+            <h1>Make a Donation or Bequest</h1>
+            <p>If you wish to donate to Friends of Battye Library, lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <p>If you wish to make a provision in your Will leaving a bequest to Friends of Battye Library Inc., please see the brochue attached for further information.</p>
+            <a href="#" class="button"><i class="fas fa-paperclip"></i> Gift Bequest Brochure (PDF)</a>
+        </section>
+    </div> <!-- end .column-right -->
 </main>
 <?php include 'templates/footer.php';?>
