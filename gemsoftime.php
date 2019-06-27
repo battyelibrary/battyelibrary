bbfd666478c19c5676fa5dc1d2a5820083c47320
Jasmine Vout <47492753+jasminevout@users.noreply.github.com>
2019-06-27 10:37:03 +0800

    Add accordion & sidebar to Gems of Time

--- gemsoftime.php
+++ gemsoftime.php
@@ -1,7 +1,57 @@
 <?php 
 $pageTitle ="Gems of Time";
 include 'templates/header.php';?>
-<main>
+<main class="grid-x">
+<!-- GEMS OF TIME -->
+    <!-- LEFT COLUMN -->
+    <div class="column column-left cell small-12 medium-8">
+        <section class="mod cell small-12 mod-section-gems">
+            <h1>Gems of Time Recipients</h1>
+            <div class="mod-section-gems-container">
+                <ul class="accordion mod-section-gems-accordion" data-accordion data-allow-all-closed="true">
+                    <!-- David Whiteford -->
+                    <li class="accordion-item mod-section-gems-accordion-item" data-accordion-item>
+                        <a href="#" class="accordion-title mod-section-gems-accordion-title">David Whiteford (2017)</a>
+                        <div class="accordion-content mod-section-gems-accordion-content" data-tab-content>
+                            <p>Member Description</p>
+                        </div>
+                    </li>
 
+                    <!-- Bevan Carter -->
+                    <li class="accordion-item mod-section-gems-accordion-item" data-accordion-item>
+                        <a href="#" class="accordion-title mod-section-gems-accordion-title">Bevan Carter (2015)</a>
+                        <div class="accordion-content mod-section-gems-accordion-content" data-tab-content>
+                            <p>Bevan Carter was born in Western Australia's wheat-belt, but holds degrees in Science and Education from the University of Sydney, as well as a Graduate Diploma in Computing from Curtin University. He taught mathematics and science at both senior high school and TAFE before retiring in the early 1990's. That is when we think his history and genie bug properly hatched, though he first did his bit for the community becoming Councilor and then Mayor of the City of Bassendean.</p>
+                            <p>In 2000 he coordinated the West Swan Landcare project and co-wrote The Vanishing Floral Heritage of the Swan Valley. The following year he produced Robert Brown: Botanist with Mathew Flinders. 2001 also saw the publication of Memories of Bassendean for its centenary - and who better to tell the stories.</p>
+                            <p>His has been a familiar face in both the State Library of Western Australia and the State Records Office of Western Australia, as well as in other research institutions in Great Britain. His trademark red cap are legendary.</p>
+                            <p>Bevan's contribution to the Battye Library can been seen in publications such as; Eden Hill Camps: records of racism in Western Australia, Nyungah land: records of the invasion and theft of aboriginal land on the Swan River 1829-1850, and his family history Norfolk poacher to Northam Farmer.</p>
+                            <p>His work with wife Jennie in producing Bassendean: a social history 1829-1979 and Settlement to City: A history of the Armadale district and currently the history of King Edward Hospital has resulted in books of considerable standing. All Bevan's work is thoroughly researched and his painstaking effort in tracing convict records and particularly Aboriginal records has made him an invaluable source of help for any new researcher - help which he gives willingly both in the Family History section of the library and round the coffee table. His continued sharing of research with others make him a very special person.</p>
+                            <p>The Friends of Battye Library Inc. would like to show our appreciation of Bevan's commitment and generosity by listing him as a "Gem of Time" - an award that celebrates living treasures who have had a long-term and significant commitment to the Battye Library and the State Records Office. The list of Gems will last to acknowledge the contribution of outstanding men and women to the recording, collection, and preservation of Western Australia's history.</p>
+                        </div>
+                    </li>
+
+                    <!-- Member Name -->
+                    <li class="accordion-item mod-section-gems-accordion-item" data-accordion-item>
+                        <a href="#" class="accordion-title mod-section-gems-accordion-title">Member Name (YYYY)</a>
+                        <div class="accordion-content mod-section-gems-accordion-content" data-tab-content>
+                            <p>Member Description</p>
+                        </div>
+                    </li>
+                </ul> <!-- end .mod-section-gems-accordion -->
+            </div> <!-- end .mod-section-gems-container -->
+        </section> <!-- end .mod-section-gems -->
+    </div> <!-- end .column-left -->
+
+    <!-- RIGHT COLUMN -->
+    <div class="column column-right cell small-12 medium-4">
+        <!-- about gems of time -->
+        <section class="mod cell small-12">
+            <h1>About Gems of Time</h1>
+            <p>
+            Presented by the Friends of Battye Committee acknowledging Western Australia's living treasures who have made a long-term and significant commitment to the aims and objectives of the Battye Library and/or the State Records office.
+            </p>
+            <img src="https://placehold.it/350x700/999?text=Gems+of+Time+Plaque" alt="Gems of Time Plaque">
+        </section>
+    </div> <!-- end .column-right -->
 </main>
 <?php include 'templates/footer.php';?>
